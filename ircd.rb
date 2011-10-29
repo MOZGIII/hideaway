39519a38cca0d13f1c31fe0c95993ebb013264f4
MOZGIII <mike-n@narod.ru>
2011-10-29 13:44:11 +0400

    Added server directory to include path for Ruby 1.9.2 support.

--- ircd.rb
+++ ircd.rb
@@ -27,6 +27,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+$: << File.dirname(File.realpath(__FILE__))
+
 require 'rubygems'
 require 'yaml'
 require 'daemons'
